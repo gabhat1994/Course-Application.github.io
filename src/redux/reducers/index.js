a83e326b839b1ad5dc467d0b160667ea6b3ef20c
stamic1 <bhatgaurav96@gmail.com>
2020-07-05 14:48:02 +0530

    redux setup

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux"
+import  courseReducer from "./courseReducer"
+
+
+const rootReducer = combineReducers({
+    courseReducer:courseReducer
+})
+
+export default rootReducer ;
